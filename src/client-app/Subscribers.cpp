40fb1bdaec574fe78e45d91e98843b6f9091f95b
Patryk Jaworski <regalis@regalis.com.pl>
2013-01-22 00:08:23 +0100

    Update Subscribers class, use own TableModel

--- src/client-app/Subscribers.cpp
+++ src/client-app/Subscribers.cpp
@@ -6,7 +6,7 @@
 #include "Subscribers.hpp"
 
 Subscribers::Subscribers(QWidget *parent) : QWidget(parent) {
-	model = new QSqlTableModel();
+	model = new TableModel();
 	model->setTable("subscribers");
 	model->setEditStrategy(QSqlTableModel::OnFieldChange);
 	model->select();
@@ -24,83 +24,20 @@ Subscribers::Subscribers(QWidget *parent) : QWidget(parent) {
 
 	table_view = new QTableView(this);
 	table_view->setModel(model);
+	table_view->resizeColumnsToContents();
 
 	QVBoxLayout *layout = new QVBoxLayout(this);
 	layout->addWidget(table_view);
 	setLayout(layout);
-	
 }
 
-bool Subscribers::setName(int id, const QString &name) {
-	QSqlQuery query;
-	query.prepare("update subscribers set name=? where id=?");
-	query.addBindValue(name);
-	query.addBindValue(id);
-	return query.exec();
+Subscribers::TableModel::TableModel() : QSqlTableModel() {}
+
+QString Subscribers::TableModel::selectStatement() const {
+	QString sql = QString("select id, name, surname, email, country, province, city, address, sex, birthday from %1").arg(tableName());
+	if (!filter().isEmpty())
+		sql += QString(" where %1");
+	sql += orderByClause();
+	return sql;
 }
 
-bool Subscribers::setSurname(int id, const QString &surname) {
-	QSqlQuery query;
-	query.prepare("update subscribers set surname=? where id=?");
-	query.addBindValue(surname);
-	query.addBindValue(id);
-	return query.exec();
-}
-
-bool Subscribers::setEmail(int id, const QString &email) {
-	QSqlQuery query;
-	query.prepare("update subscribers set email=? where id=?");
-	query.addBindValue(email);
-	query.addBindValue(id);
-	return query.exec();
-}
-
-bool Subscribers::setCountry(int id, const QString &country) {
-	QSqlQuery query;
-	query.prepare("update subscribers set country=? where id=?");
-	query.addBindValue(country);
-	query.addBindValue(id);
-	return query.exec();
-}
-
-bool Subscribers::setProvince(int id, const QString &province) {
-	QSqlQuery query;
-	query.prepare("update subscribers set province=? where id=?");
-	query.addBindValue(province);
-	query.addBindValue(id);
-	return query.exec();
-}
-
-bool Subscribers::setCity(int id, const QString &city) {
-	QSqlQuery query;
-	query.prepare("update subscribers set city=? where id=?");
-	query.addBindValue(city);
-	query.addBindValue(id);
-	return query.exec();
-}
-
-bool Subscribers::setAddress(int id, const QString &address) {
-	QSqlQuery query;
-	query.prepare("update subscribers set address=? where id=?");
-	query.addBindValue(address);
-	query.addBindValue(id);
-	return query.exec();
-}
-
-bool Subscribers::setSex(int id, const QString &sex) {
-	if (sex != "M" || sex != "F")
-		return false;
-	QSqlQuery query;
-	query.prepare("update subscribers set sex=? where id=?");
-	query.addBindValue(sex);
-	query.addBindValue(id);
-	return query.exec();
-}
-
-bool Subscribers::setBirthday(int id, const QString &birthday) {
-	QSqlQuery query;
-	query.prepare("update subscribers set birthday=? where id=?");
-	query.addBindValue(birthday);
-	query.addBindValue(id);
-	return query.exec();
-}
--- src/client-app/Subscribers.hpp
+++ src/client-app/Subscribers.hpp
@@ -8,18 +8,17 @@
 class Subscribers : public QWidget {
 	public:
 		Subscribers(QWidget *parent);
-		static bool setName(int id, const QString &name);
-		static bool setSurname(int id, const QString &surname);
-		static bool setEmail(int id, const QString &email);
-		static bool setCountry(int id, const QString &country);
-		static bool setProvince(int id, const QString &province);
-		static bool setCity(int id, const QString &city);
-		static bool setAddress(int id, const QString &address);
-		static bool setSex(int id, const QString &sex);
-		static bool setBirthday(int id, const QString &birthday);
 	protected:
-		QSqlTableModel *model;
+		class TableModel;
+		TableModel *model;
 		QTableView *table_view;
+};
+
+class Subscribers::TableModel : public QSqlTableModel {
+	public:
+		TableModel();
+	protected:
+		QString selectStatement() const;
 };
 
 #endif
