880fb853345f8ce6d6f6b357023d4fb228f26c1c
Ariana Las <ariana.las@gmail.com>
2013-01-22 16:49:02 +0100

    Dashboard prepared to display

--- src/client-app/Dashboard.cpp
+++ src/client-app/Dashboard.cpp
@@ -5,5 +5,8 @@
 #include "version.hpp"
 
 Dashboard::Dashboard(QWidget *parent) : QWidget(parent) {
+	vbox = new QVBoxLayout(this);
 	QLabel *hello_message = new QLabel(QString("Welcome in Regalis Newsletter v%1").arg(REGALIS_NEWSLETTER_VERSION));
+	vbox->addWidget(hello_message, 0, Qt::AlignHCenter);
+	setLayout(vbox);
 }
--- src/client-app/Dashboard.hpp
+++ src/client-app/Dashboard.hpp
@@ -2,9 +2,12 @@
 #define __DASHBOARD_HPP_
 
 #include <QWidget>
+#include <QVBoxLayout>
 
 class Dashboard : public QWidget {
 	public:
 		Dashboard(QWidget *parent);
+	protected:
+		QVBoxLayout *vbox;
 };
 #endif
