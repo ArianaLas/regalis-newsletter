a47a5c7d1fe5d5df29b9c5319bcf48fa690a6479
Ariana Las <ariana.las@gmail.com>
2013-02-05 14:32:02 +0100

    Modernized preferences files

--- src/client-app/Preferences.cpp
+++ src/client-app/Preferences.cpp
@@ -2,42 +2,99 @@
 #include <QString>
 #include <QGroupBox>
 #include <QGridLayout>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QMessageBox>
+#include <QSqlError>
+#include <QSqlRecord>
+#include <iostream>
+#include <QCheckBox>
+#include <QLineEdit>
+#include <QComboBox>
 
 #include "Preferences.hpp"
 
 Preferences::Preferences(QWidget *parent) : QWidget(parent) {
 	setWindowTitle(tr("Preferences"));
 	vbox = new QVBoxLayout(this);
-	init();
 	setLayout(vbox);
+	form_model = new QSqlTableModel();
+	form_model->setTable("settings");
+	form_model->setFilter("name like 'register\_form\_%'");
+	form_model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+
+	form_mapper = new QDataWidgetMapper();
+	form_mapper->setModel(form_model);
+	form_mapper->setOrientation(Qt::Vertical);
+}
+
+void Preferences::show() {
+	if (form_model->select()) {
+		form_mapper->setCurrentIndex(3);
+	std::cout << "_______________________________________" << std::endl;
+	std::cout << form_model->rowCount() << std::endl;
+		for (int i = 0; i < form_model->rowCount(); ++i) {
+			std::cout << form_model->record(i).value("name").toString().toUtf8().constData() << std::endl;
+		}
+	std::cout << "_______________________________________" << std::endl;
+		init();
+		QWidget::show();
+	} else {
+		QMessageBox::critical(0, tr("Unable to load configuration"), QString(tr("Unable to get configuration from database: %1")).arg(form_model->lastError().text()));
+	}
+}
+
+int Preferences::getRowFromName(const QString &name) const {
+	for (int i = 0; i < form_model->rowCount(); ++i) {
+	//	std::cout << form_model->record(i).value("name").toString().toUtf8().constData() << std::endl;
+		if (name == form_model->record(i).value("name").toString()) {
+	//		std::cout << "id: " << i << std::endl;
+
+			return i;
+		}
+	}
+	return -1;
 }
 
 void Preferences::init() {
+
 	QGroupBox *core_box = new QGroupBox(tr("Core settings"));
 	QGroupBox *form_box = new QGroupBox(tr("Form settings"));
 
 	QGridLayout *core_layout = new QGridLayout();
 	QGridLayout *form_layout = new QGridLayout();
 
-	enable_email_confirmation = new QCheckBox(tr("Email confirmation"));
-	enable_auto_email_confirmation = new QCheckBox(tr("Auto email confirmation"));
+	QCheckBox *enable_email_confirmation = new QCheckBox(tr("Email confirmation"));
+	QCheckBox *enable_auto_email_confirmation = new QCheckBox(tr("Auto email confirmation"));
 
-	auto_email_confirmation_template = new QComboBox();
-	email_unregister_template = new QComboBox();
-	auto_email_confirmation_account = new QComboBox();
+	QComboBox *auto_email_confirmation_template = new QComboBox();
+	QComboBox *email_unregister_template = new QComboBox();
+	QComboBox *auto_email_confirmation_account = new QComboBox();
 
-	enable_email_mx_verification = new QCheckBox(tr("Email MX verification"));
-	register_form_name_required = new QCheckBox(tr("Name"));
-	register_form_surname_required = new QCheckBox(tr("Surname"));
-	register_form_city_required = new QCheckBox(tr("City"));
-	register_form_province_required = new QCheckBox(tr("province"));
-	register_form_country_required = new QCheckBox(tr("Country"));
-	register_form_birthday_required = new QCheckBox(tr("Birthday"));
-	register_form_language_required = new QCheckBox("Language");
+	QCheckBox *enable_email_mx_verification = new QCheckBox(tr("Email MX verification"));
+	QCheckBox *register_form_name_required = new QCheckBox(tr("Name"));
+	QCheckBox *register_form_surname_required = new QCheckBox(tr("Surname"));
+	QCheckBox *register_form_city_required = new QCheckBox(tr("City"));
+	QCheckBox *register_form_province_required = new QCheckBox(tr("Province"));
+	QCheckBox *register_form_country_required = new QCheckBox(tr("Country"));
+	QCheckBox *register_form_birthday_required = new QCheckBox(tr("Birthday"));
+	QCheckBox *register_form_language_required = new QCheckBox("Language");
+	QCheckBox *register_form_sex_required = new QCheckBox("Sex");
+	QCheckBox *register_form_use_campaigns = new QCheckBox("Use campaigns");
+	QCheckBox *register_form_save_user_agent = new QCheckBox("Save user agent");
 
+	form_mapper->addMapping(register_form_name_required, getRowFromName("register_form_name_required"));
+	std::cout << form_mapper->mappedSection(register_form_name_required) << std::endl;
+	form_mapper->addMapping(register_form_surname_required, getRowFromName("register_form_surname_required"));
+	form_mapper->addMapping(register_form_city_required, getRowFromName("register_form_city_required"));
+	form_mapper->addMapping(register_form_province_required, getRowFromName("register_form_province_required"));
+	form_mapper->addMapping(register_form_country_required, getRowFromName("register_form_country_required"));
+	form_mapper->addMapping(register_form_birthday_required, getRowFromName("register_form_birthday_required"));
+	form_mapper->addMapping(register_form_language_required, getRowFromName("register_form_language_required"));
+	form_mapper->addMapping(register_form_sex_required, getRowFromName("register_form_sex_required"));
+	form_mapper->addMapping(register_form_use_campaigns, getRowFromName("register_form_use_campaigns"));
+	form_mapper->addMapping(register_form_save_user_agent, getRowFromName("register_form_save_user_agent"));
 	
-
-
 	QLabel *auto_email_confirmation_template_label = new QLabel(tr("Auto email confirmation template"));
 	QLabel *email_unregister_template_label = new QLabel(tr("Email unregister template"));
 	QLabel *auto_email_confirmation_account_label = new QLabel(tr("Auto email confirmation account"));
@@ -53,7 +110,7 @@ void Preferences::init() {
 	core_layout->addWidget(auto_email_confirmation_account, 4, 0);
 	core_layout->addWidget(enable_email_mx_verification, 5, 0);
 
-	QLabel *form_label = new QLabel(tr("Select fields below if required"));
+	QLabel *form_label = new QLabel(tr("Select fields below if required in registration form"));
 	form_layout->addWidget(form_label, 0, 0, 1, 2);
 	form_layout->addWidget(register_form_name_required,1, 0);
 	form_layout->addWidget(register_form_surname_required, 1, 1);
@@ -62,10 +119,24 @@ void Preferences::init() {
 	form_layout->addWidget(register_form_country_required, 3, 0);
 	form_layout->addWidget(register_form_birthday_required, 3, 1);
 	form_layout->addWidget(register_form_language_required, 4, 0);
+	form_layout->addWidget(register_form_sex_required, 4, 1);
+	form_layout->addWidget(register_form_use_campaigns, 5, 0);
+	form_layout->addWidget(register_form_save_user_agent, 5, 1);
 
 	core_box->setLayout(core_layout);
 	form_box->setLayout(form_layout);
 
 	vbox->addWidget(core_box);
 	vbox->addWidget(form_box);
+
+	QPushButton *cancel = new QPushButton("Cancel");
+	QPushButton *ok = new QPushButton("Ok");
+
+	QHBoxLayout *hbox = new QHBoxLayout();
+
+	hbox->addStretch();
+	hbox->addWidget(cancel);
+	hbox->addWidget(ok);
+
+	vbox->addLayout(hbox);
 }
--- src/client-app/Preferences.hpp
+++ src/client-app/Preferences.hpp
@@ -3,28 +3,21 @@
 
 #include <QWidget>
 #include <QVBoxLayout>
-#include <QCheckBox>
-#include <QComboBox>
+#include <QSqlTableModel>
+#include <QDataWidgetMapper>
 
 class Preferences : public QWidget {
 	public:
 		Preferences(QWidget *parent = 0);
+		void show();
 
 	protected:
-		QVBoxLayout *vbox;
 		void init();
-		QCheckBox *enable_email_confirmation; 
-		QCheckBox *enable_auto_email_confirmation;
-		QComboBox *auto_email_confirmation_template;
-		QComboBox *email_unregister_template;
-		QComboBox *auto_email_confirmation_account;
-		QCheckBox *enable_email_mx_verification;
-		QCheckBox *register_form_name_required;
-		QCheckBox *register_form_surname_required;
-		QCheckBox *register_form_city_required;
-		QCheckBox *register_form_province_required;
-		QCheckBox *register_form_country_required;
-		QCheckBox *register_form_birthday_required;
-		QCheckBox *register_form_language_required;
+		int getRowFromName(const QString &name) const;
+		
+		QSqlTableModel *form_model;
+		QDataWidgetMapper *form_mapper;
+
+		QVBoxLayout *vbox;
 };
 #endif
